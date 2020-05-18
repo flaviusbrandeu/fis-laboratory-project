6a658ac8eced5b6e11734ba338e715fd975f378b
Flavius-Gabriel Brandeu <flavius.brandeu@gmail.com>
2020-05-18 18:02:37 +0300

    #FTA-15 created the user interface for login (login screen)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,26 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("Football Team Application");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        Label label=new Label("Football Team Application");
-        Scene scene=new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
