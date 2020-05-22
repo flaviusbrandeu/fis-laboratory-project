924fbaa6e65e332a153e9a52b22c189d7b2701bc
Flavius-Gabriel Brandeu <flavius.brandeu@gmail.com>
2020-05-22 21:24:08 +0300

    #FTA-17 Created differentiated menus(just the window) for the football agent and for the team manager

--- src/main/java/org/fis/student/football/team/application/login/controllers/LoginController.java
+++ src/main/java/org/fis/student/football/team/application/login/controllers/LoginController.java
@@ -2,17 +2,18 @@ package org.fis.student.football.team.application.login.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import org.fis.student.football.team.application.login.exceptions.UserDoesNotExistException;
 import org.fis.student.football.team.application.login.exceptions.WrongCredentialsException;
 import org.fis.student.football.team.application.login.services.UserService;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -26,6 +27,14 @@ public class LoginController implements Initializable {
     private ChoiceBox<String> role;
     @FXML
     private Button loginButton;
+    @FXML
+    private StackPane rootPane;
+    @FXML
+    private GridPane loginGridPane;
+    @FXML
+    private GridPane teamManagerMenu;
+    @FXML
+    private GridPane footballAgentMenu;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -34,15 +43,27 @@ public class LoginController implements Initializable {
 
 
     @FXML
-    public void handleLoginAction(ActionEvent event){
+    public void handleLoginAction(ActionEvent event) throws Exception{
+        teamManagerMenu= FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("teamManagerMenu.fxml")));
+        footballAgentMenu=FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("footballAgentMenu.fxml")));
         try{
             UserService.verifyCredentials(usernameField.getText(),passwordField.getText(),role.getValue());
-            loginMessage.setText("You're logged in!");
+
+            switch (role.getValue()){
+                case "Team Manager":
+                    rootPane.getChildren().add(teamManagerMenu);
+                    break;
+                case "Football Agent":
+                    rootPane.getChildren().add(footballAgentMenu);
+                    break;
+            }
+            rootPane.getChildren().remove(loginGridPane);
         } catch(UserDoesNotExistException e){
             loginMessage.setText(e.getMessage());
         } catch (WrongCredentialsException e){
             loginMessage.setText(e.getMessage());
         }
+
 //        switch (role.getValue())
 //        {
 //            case "Team Manager":
