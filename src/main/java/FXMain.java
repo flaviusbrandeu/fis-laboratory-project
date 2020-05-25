695b61b8d79cf24b4244fccb3ffbb091d1555b8e
Flavius-Gabriel Brandeu <flavius.brandeu@gmail.com>
2020-05-25 13:18:18 +0300

    Modified project to generate fatjar

---
+++ src/main/java/FXMain.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.fis.student.football.team.application.login.services.UserService;
+
+import java.util.Objects;
+
+public class FXMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        UserService.loadUsersFromFile();
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("loginScreen.fxml")));
+        primaryStage.setTitle("Football Team Application");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,6 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.fis.student.football.team.application.login.services.UserService;
-
-import java.util.Objects;
-
-public class Main extends Application {
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        UserService.loadUsersFromFile();
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("loginScreen.fxml")));
-        primaryStage.setTitle("Football Team Application");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+        FXMain.main(args);
     }
 }
